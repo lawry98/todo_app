86e0f525fffa02f347a643afef1c8df538fe7098
Lawrence Crasto <lawry1998@gmail.com>
2024-09-09 17:09:28 -0400

    added useEffect

--- frontend/components/displayTodo.jsx
+++ frontend/components/displayTodo.jsx
@@ -3,7 +3,7 @@ export function DisplayTodo({todo}){
         <div>
             <br></br>
             <div>{todo[0].title}</div>
-            <div>{todo[0].title}</div>
+            <div>{todo[0].description}</div>
             <div>{todo[0].completed==true ? "completed" : "mark as done"}</div>
         </div>
     )
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,15 @@ import { CreateTodo } from '../components/createTodo'
 import { DisplayTodo } from '../components/displayTodo'
 
 function App() {
-  const [todos, setTodo] = useState([]);
+  const [todos, setTodo] = useState([{title:"", description:"", completed: false}]);
   useEffect(()=> {
-    //setTodo([{title:"go to gym", description:"workout", completed: false}]);
+    setTodo([{title:"go to gym2", description:"workout2", completed: false}]);
+    alert("hello");
   }, [])
   return (
     <div>
        <CreateTodo></CreateTodo>
-       <DisplayTodo todo={[{title:"go to gym", description:"workout", completed: false}]}></DisplayTodo>
+       <DisplayTodo todo={todos}></DisplayTodo>
     </div>
   )
 }
